bd95e205f8d4a8681e958f233aeaa3d75c6ff7dd
vasabi07 <vasanthanarulselvan@gmail.com>
2025-06-20 03:38:26 +0530

    Initial commit with frontend and backend folders

---
+++ py-backend/main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello from py-backend!")
+
+
+if __name__ == "__main__":
+    main()
+"""
+OCR based text extraction using unstructed.io
+use langchain/llamaindex for ingest n retrieval 
+"""
